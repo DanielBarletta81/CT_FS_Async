ef9f7a49ae9d3770914a262bc406671072ac9c45
DanielBarletta81 <dbarcelona1481@gmail.com>
2025-01-23 00:19:41 -0500

    initial upload

---
+++ marvel.js
@@ -0,0 +1,34 @@
+//Task 1: Obtaining API Key and Configuration
+/* 
+Obtain an API key from the Marvel Comics API website. Investigate and understand 
+the necessary configurations required to authenticate requests to the API. 
+You would need to generate a hash based off of the provide key combo 
+in the documentation. 
+
+I would recommend using https://www.md5hashgenerator.com/ to generate the hash.
+*/
+
+const apiKey = "YOUR_API_KEY";
+const apiHash = "YOUR_API_HASH";
+const apiUrl = "https://gateway.marvel.com/v1/public/characters";
+
+
+/*
+
+Task 2: Fetching Characters Using Fetch API
+
+Implement a function to fetch Marvel Comics characters asynchronously from the API 
+endpoint using the Fetch API and promises. Utilize the API key 
+and configurations obtained in Task 1. Log the fetched characters to the console.
+
+*/
+
+/*
+
+Task 3: Updating User Interface Dynamically
+
+Write a function to dynamically update the user interface with 
+the fetched characters' information. 
+Utilize promises and the Fetch API to ensure that the UI updates 
+only after the characters are successfully fetched. */
+
